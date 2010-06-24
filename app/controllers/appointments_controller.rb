794e0446074354a944b6ec2cd077f0db756bf693
Brian Swan <brianswan@Brian-Swans-Mac-Pro.local>
2010-06-24 17:28:55 +0100

    Added create appointments view and controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,4 +4,10 @@ class AppointmentsController < ApplicationController
     @appointments = Appointment.all
   end
   
+  def create
+    Appointment.create(params[:appointment])
+    
+    redirect_to appointments_path
+  end
+  
 end
--- spec/controllers/appointments_controller_spec.rb
+++ spec/controllers/appointments_controller_spec.rb
@@ -13,4 +13,16 @@ describe AppointmentsController do
     assert_match /Dave.*fluffy/, response.body
     assert_match /John.*rex/, response.body
   end
+  
+  it "should create a new appointment and display it on the index page" do
+    
+    post :create, "appointment"=>{"patient"=>"fluffy", "owner"=>"dave"}
+    
+    assert_redirected_to appointments_path
+    
+    appointment = Appointment.find_by_patient("fluffy")
+    assert_not_nil appointment
+    assert_equal "fluffy", appointment.patient
+    assert_equal "dave", appointment.owner
+  end
 end
