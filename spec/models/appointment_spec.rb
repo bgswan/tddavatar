a6a78ba3ca15837b107dc5ba80ade2162a516204
Brian Swan <brianswan@Brian-Swans-Mac-Pro.local>
2010-06-29 13:55:58 +0100

    Added failing test for total due on appointment

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -27,5 +27,19 @@ describe Appointment do
     assert_equal 10.0, appointment.payments.first.amount
   end
   
-  it "can calculate total due"
+  it "can calculate total due" do
+    assert_equal 0, appointment.total_due
+    
+    appointment.charges << ChargeDescription.create(:treatment => 'Rabies shot', :cost => 50.0 ).new_charge
+    
+    assert_equal 50.0, appointment.total_due
+    
+    appointment.payments << Payment.new(:amount => 30.0)
+    
+    assert_equal 20.0, appointment.total_due
+    
+    appointment.payments << Payment.new(:amount => 20.0)
+    
+    assert_equal 0, appointment.total_due
+  end
 end
