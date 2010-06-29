edf7ae0f05f0925dac394815a50ea586c0c99305
Brian Swan <brianswan@Brian-Swans-Mac-Pro.local>
2010-06-29 13:14:48 +0100

    Updated to view to include form to add treatments

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100628120406) do
+ActiveRecord::Schema.define(:version => 20100629121039) do
 
   create_table "appointments", :force => true do |t|
     t.string   "owner"
